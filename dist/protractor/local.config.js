4d66b80923ad754d5e55b2b391d7a7fd9e86cf4e
sednarvaezna <sednarvaezna@unal.edu.co>
2021-09-19 01:23:10 -0500

    setup protractor configuration

---
+++ dist/protractor/local.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    framework: 'jasmine',
+    specs: ['../test/google.spec.js'],
+    seleniumAddress: 'http://localhost:4444/wd/hub'
+};
+//# sourceMappingURL=local.config.js.map
